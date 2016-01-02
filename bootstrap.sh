4044c67c9083baf6b81fa854f50c865bf9cce816
pigworlds <xxpig@hotmail.com>
2016-01-02 00:18:47 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,7 @@ tar xvjf dscpmod.tar.bz
 chmod a+x iptables
 insmod xt_DSCP.ko
 ./iptables -t mangle -A PREROUTING -i `nvram get wan_ifnames` -j DSCP --set-dscp 0
+./iptables -t mangle -A POSTROUTING -o `nvram get wan_ifnames` -j DSCP --set-dscp-class EF
 
 # dump iptables
 ./iptables -t mangle -L
